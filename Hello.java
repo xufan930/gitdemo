1fc0916dfb4bda60fe3ff6f5c7c813947226f6a7
xufan930 <390995324@qq.com>
2018-08-13 15:09:25 +0800

    java测试

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello{
     public static void main (String[] args) throws java.lang.Exception{
-        System.out.println("hello world!");
+        System.out.println("hello world!!!");
     }
 }
